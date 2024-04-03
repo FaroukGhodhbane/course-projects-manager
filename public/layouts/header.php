2d04c7ba0df353270990f14df6caeb9e7f80490a
Farouk Ghodhbane <fghodbane99@gmail.com>
2024-04-03 16:00:55 -0700

    Fixed title tag on head tag

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Projects Manager</title>
+    <title>Course Projects Manager</title>
     <link rel="stylesheet" href="view/css/main.min.css">
     <style>
         .bold {
